bc7a6cb282a4b5de98672b2316a54cc7cc1b7efe
Wim Heuts <wim.heuts@planonsoftware.com>
2015-07-24 16:50:34 +0200

    Some small changes

--- jenkins/jenkins.bat
+++ jenkins/jenkins.bat
@@ -1,6 +1,6 @@
 call D:\Sandboxes\setant.bat
 echo ========
-echo Start Jenkins webclient in a few seconds at: http://localhost:8080/
+echo Start Jenkins webclient in a few seconds at: http://localhost:9090/
 echo ========
 %java_home%\bin\java -jar jenkins.war --httpPort=9090
 pause
